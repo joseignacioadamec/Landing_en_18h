649d702d610556bc72a007e9567c4ac69bcf487c
José Ignacio <joseignacioadamec@gmail.com>
2024-04-15 13:48:58 +0200

    new build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
   },
 });
